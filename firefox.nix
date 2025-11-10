57ab81c56c421da796ace7f5915461ae386b37f6
Thomas Collerton <tho.collerton@gmail.com>
2025-11-10 20:26:15 +0100

    add firefox addons

--- firefox.nix
+++ firefox.nix
@@ -1,4 +1,29 @@
-{ pkgs, ...}:
+{ inputs, pkgs, lib, ...}:
+
 {
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      id = 0;
+      name = "default";
+      isDefault = true;
+      search = {
+        default = "ddg";
+        order = [ "ddg" "google" ];
+        force = true;
+        engines = {
+          bing.metaData.hidden = true;
+        };
+      };
+      extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
+        ublock-origin
+        privacy-badger
+        proton-pass
+        proton-vpn
+      ];
+      settings = {
+        "browser.startup.page" = 3; # Restore previous session's tabs
+      };
+    };
+  };
 }
