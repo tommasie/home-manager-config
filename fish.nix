a3a59bbf121a1653509b3a93d55da4149d7282a1
Thomas Collerton <tho.collerton@gmail.com>
2025-11-10 20:26:15 +0100

    add fish config

---
+++ fish.nix
@@ -0,0 +1,21 @@
+{
+  programs.fish = {
+    enable = true;
+    plugins = [
+      {
+        name = "grc";
+        src = pkgs.fishPlugins.grc.src;
+      }
+      {
+        name = "fish-ssh-agent";
+        src = pkgs.fetchFromGitHub {
+          owner = "danhper";
+          repo = "fish-ssh-agent";
+          rev = "f10d957";
+          sha256 = "10c0sg5nyh36mk2xlnxw9fw00w8yraj11nbwhm0rw1fjnd1yhnkh";
+        };
+      }
+    ];
+  };
+
+}
