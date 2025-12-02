c4cc05fe6f1c711ae6b23b05c8dfb866dec85e52
Thomas Collerton <tho.collerton@gmail.com>
2025-12-02 19:33:06 +0100

    disable hyprland + update

--- home.nix
+++ home.nix
@@ -18,10 +18,6 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    pkgs.hello
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -68,13 +64,14 @@
   #  /etc/profiles/per-user/thomas/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
+    GH_TOKEN = "";
   };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   imports = [
-    ./hyprland.nix
+    # ./hyprland.nix
     ./firefox.nix
     ./nvim.nix
     ./git.nix
