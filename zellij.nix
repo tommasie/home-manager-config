c1f4f60513291841769212f225d46994ffdc469c
Thomas Collerton <tho.collerton@gmail.com>
2026-03-16 23:30:28 +0100

    flake update

--- zellij.nix
+++ zellij.nix
@@ -4,7 +4,9 @@
     enableFishIntegration = true;
     exitShellOnExit = true;
     settings = {
+      # use Wayland clipboard
       copy_command = "wl-copy";
+      show_startup_tips = false;
     };
   };
 }
