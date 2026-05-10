b50706c1325886d26b140a1da0702d54964339c3
Thomas Collerton <tho.collerton@gmail.com>
2026-05-10 09:12:16 +0200

    update firefox profile location

--- home.nix
+++ home.nix
@@ -74,6 +74,8 @@
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
+  xdg.enable = true;
+
   dconf = {
     enable = true;
     settings."org/gnome/shell" = {
